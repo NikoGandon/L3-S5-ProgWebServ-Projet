d78d989f57438ea510f5e613d20b76637c12804d
NikoGandon <nikola.gandon@gmail.com>
2023-11-30 11:19:00 +0100

    init: passport Avancé des stratégies de login et de signup Liée par les modèles

--- ServeurSide/app-express/src/Middleware/Passport/passportRegister.js
+++ ServeurSide/app-express/src/Middleware/Passport/passportRegister.js
@@ -1,4 +1,42 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const { Op } = require("sequelize");
 
-const User = require('../../Models/User');
+const User = require("../../Models/User");
+
+/**
+ * @desc    Passport Register
+ * @param   {String} username nom d'utilisateur
+ * @param   {String} email email de l'utilisateur
+ * @param   {String} password mot de passe de l'utilisateur
+ *
+ * @return  {Object} passport
+ * 
+ */
+
+passport.use(
+  "passportRegister",
+  new LocalStrategy(async (username, email, password, next) => {
+    try {
+      const existUser = await User.findOne({
+        where: { [Op.or]: [{ username: username }, { email: email }] },
+      });
+
+      if (existUser) {
+        return next(null, false, { message: "Cet utilisateur existe déjà" });
+      }
+
+      const newUser = await User.create({
+        username: username,
+        email: email,
+        password: password
+      });
+
+      return next(null, newUser);
+    } catch (error) {
+      return next(error);
+    }
+  })
+);
+
+module.exports = passport;
