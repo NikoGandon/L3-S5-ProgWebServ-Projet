dc7498f4257423fe3ddb167adad5c83e5fc57b69
NikoGandon <nikola.gandon@gmail.com>
2023-12-02 00:45:19 +0100

    feat: établissement des routes d'authentification

--- ServeurSide/app-express/src/Route/User/Connexion/login.js
+++ ServeurSide/app-express/src/Route/User/Connexion/login.js
@@ -1,6 +1,40 @@
 const express = require("express");
 const routerLogin = express.Router();
 
-routerLogin.get("/", (req, res) => {
-  res.send("Login page");
+const passportLogin = require("../../../Middleware/Passport/passportLogin");
+
+routerLogin.post("/", (req, res) => {
+  if (!req.body.identifier || !req.body.password) {
+    res.status(400).json({
+      message: "Veuillez remplir tous les champs",
+    });
+  }
+  try {
+    passportLogin.authenticate(
+      "passportLogin",
+      {
+        identifier: req.body.identifier,
+        password: req.body.password,
+      },
+      (err, user, info) => {
+        if (err) {
+          return next(err);
+        }
+
+        if (!user) {
+          return res.status(400).json({
+            message: info.message,
+          });
+        }
+
+        return res.status(200).json({
+          message: info.message,
+        });
+      }
+    )(req, res, next);
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+module.exports = routerLogin;
--- ServeurSide/app-express/src/Route/User/Connexion/register.js
+++ ServeurSide/app-express/src/Route/User/Connexion/register.js
@@ -1,6 +1,42 @@
 const express = require("express");
 const routerRegister = express.Router();
 
-routerRegister.get("/", (req, res) => {
-  res.send("Register page");
+const passportRegister = require("../../../Middleware/Passport/passportRegister");
+
+routerRegister.post("/", (req, res, next) => {
+  if (!req.body.username || !req.body.email || !req.body.password) {
+    res.status(400).json({
+      message: "Veuillez remplir tous les champs",
+    });
+  }
+
+  try {
+    passportRegister.authenticate(
+      "passportRegister",
+      {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      },
+      (err, user, info) => {
+        if (err) {
+          return next(err);
+        }
+
+        if (!user) {
+          return res.status(400).json({
+            message: info.message,
+          });
+        }
+
+        return res.status(200).json({
+          message: info.message,
+        });
+      }
+    )(req, res, next);
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+module.exports = routerRegister;
