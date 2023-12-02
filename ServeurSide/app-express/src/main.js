89025901e9e76b700cf9b770703b93b0aca1006c
NikoGandon <nikola.gandon@gmail.com>
2023-12-02 14:21:55 +0100

    feat: utilisation module HTTPS & fs

--- ServeurSide/app-express/src/main.js
+++ ServeurSide/app-express/src/main.js
@@ -1,16 +1,25 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const https = require("https")
+const fs = require("fs")
+
+const optionsSSL = {
+    key: fs.readFileSync("./ServeurFolder/SSL_Certificat/private-key.pem"),
+    cert: fs.readFileSync("./ServeurFolder/SSL_Certificat/certificate.pem")
+}
+
+https.createServer(optionsSSL, app).listen(process.env.PORT, () => {
+    console.log(`Serveur lancé sur le port ${process.env.PORT}`)
+    }
+);
+
 
 // routes
 const homeRoute = require("./Route/home")
 const registerRoute = require("./Route/User/Authentification/register")
 const loginRoute = require("./Route/User/Authentification/login")
 
-app.listen(process.env.PORT, () => {
-    console.log(`Serveur lancé sur le port ${process.env.PORT}`)
-    }
-); 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
