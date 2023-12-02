65fc4825c6cd1b680854f8600fb352de1716c382
NikoGandon <nikola.gandon@gmail.com>
2023-12-03 00:44:26 +0100

    fix: main

--- ServeurSide/app-express/src/main.js
+++ ServeurSide/app-express/src/main.js
@@ -1,34 +1,42 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const https = require("https");
+const fs = require("fs");
+const session = require("express-session");
+const passport = require("passport");
 const bodyParser = require("body-parser");
 
 const optionsSSL = {
-    key: fs.readFileSync("./ServeurFolder/SSL_Certificat/private-key.pem"),
-    cert: fs.readFileSync("./ServeurFolder/SSL_Certificat/certificate.pem")
-}
+  key: fs.readFileSync("./ServeurFolder/SSL_Certificat/private-key.pem"),
+  cert: fs.readFileSync("./ServeurFolder/SSL_Certificat/certificate.pem"),
+};
 
-https.createServer(optionsSSL, app).listen(process.env.PORT, () => {
-    console.log(`Serveur lancé sur le port ${process.env.PORT}`)
-    }
-);
+const httpsServer = https.createServer(optionsSSL, app);
 
-app.use(session({
-    secret: 'coucou',
+httpsServer.listen(process.env.PORT, () => {
+  console.log("HTTPS Server running on port " + process.env.PORT);
+});
+
+app.use(
+  session({
+    secret: "coucou",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // routes
-const homeRoute = require("./Route/home")
-const registerRoute = require("./Route/User/Authentification/register")
-const loginRoute = require("./Route/User/Authentification/login")
-const GoogleOAuthRoute = require("./Route/User/Authentification/OAuth2/Google.OAuth2")
-
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const homeRoute = require("./Route/home");
+const registerRoute = require("./Route/User/Authentification/register");
+const loginRoute = require("./Route/User/Authentification/login");
+const GoogleOAuthRoute = require("./Route/User/Authentification/OAuth2/Google.OAuth2");
 
 app.use("/", homeRoute);
 app.use("/register", registerRoute);
