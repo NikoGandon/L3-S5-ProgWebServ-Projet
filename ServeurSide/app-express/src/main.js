77637d9a7adf1461e5b4b3c7722d8db1f4bbae03
NikoGandon <nikola.gandon@gmail.com>
2023-12-02 02:15:06 +0100

    WIP: ajout de route Pour tester l'authentification

--- ServeurSide/app-express/src/main.js
+++ ServeurSide/app-express/src/main.js
@@ -2,4 +2,19 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 
+// routes
+const homeRoute = require("./Route/home")
+const registerRoute = require("./Route/User/Authentification/register")
+const loginRoute = require("./Route/User/Authentification/login")
 
+app.listen(process.env.PORT, () => {
+    console.log(`Serveur lancé sur le port ${process.env.PORT}`)
+    }
+); 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", homeRoute);
+app.use("/register", registerRoute);
+app.use("/login", loginRoute);
