a6d68146318fcdc1bf3404590102a1c4cd9e272f
NikoGandon <nikola.gandon@gmail.com>
2023-12-06 22:04:32 +0100

    WIP: Route de l'utilisateur

--- ServeurSide/app-express/src/Route/User/User.js
+++ ServeurSide/app-express/src/Route/User/User.js
@@ -9,6 +9,98 @@ routerUser.post('/login', login);
 routerUser.post('/register', register);
 // ? - - routerUser.get('/OAuth/Google', OAuth);
 
+/**
+ * @swagger
+ * /User:
+ * get:
+ * description:
+ * responses:
+ * 
+ */
+routeurUser.get('/', () => {
+
+});
+
+/**
+ * @swagger
+ * /User:
+ * put:
+ * description: Modifie les paramètres de l'utilisateur 
+ * responses:
+ *
+ */
+
+routeurUser.put('/', () => {});
+
+/**
+ * @swagger
+ * /User:
+ * delete:
+ * description: Supprime l'utilisateur
+ * responses:
+ * 
+ */
+
+routeurUser.delete('/', () => {});
+
+/**
+ * @swagger
+ * /User/friend:
+ * post:
+ * description: Ajoute un ami
+ * responses:
+ *
+ */
+
+routeurUser.post('/friend', () => {});
+
+
+/**
+ * @swagger
+ * /User/friend:
+ * get:
+ * description: Récupère la liste des amis
+ * responses:
+ * 
+ */
+
+routeurUser.get('/friend', () => {});
+
+
+/**
+ * @swagger
+ * /User/friend:
+ * delete:
+ * description: Supprime un ami
+ * responses:
+ *  
+ */
+
+routeurUser.delete('/friend', () => {});
+
+
+/**
+ * @swagger
+ * /User/blocklist:
+ * post:
+ * description: Ajoute un utilisateur à sa blocklist
+ * responses: 
+ * 
+ */
+
+routeurUser.post('/blocklist', () => {});
+
+
+/**
+ * @swagger
+ * /User/blocklist:
+ * get:
+ * description: Récupère la blocklist
+ * responses:
+ * 
+ */
+
+routeurUser.get('/blocklist', () => {});
 
 
 module.exports = routerUser;
