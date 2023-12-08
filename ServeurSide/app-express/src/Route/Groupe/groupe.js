6b8b1575c215af330d37d05f91d76a690708b426
MOttet18 <113054042+MOttet18@users.noreply.github.com>
2023-12-08 14:33:42 +0100

    WIP: création de routes de groupe
    ajout de routes groupe

--- ServeurSide/app-express/src/Route/Groupe/groupe.js
+++ ServeurSide/app-express/src/Route/Groupe/groupe.js
@@ -56,4 +56,88 @@ routerGroupe.delete("/", (req, res) => {
   res.send("Suppression de groupe");
 });
 
+/**
+ * @swagger
+ * /groupe:
+ * delete:
+ * description: Utilisé pour supprimer un groupe
+ * responses:
+ * 
+ * 
+ */
+
+routerGroupe.post("/", (req, res) => {
+  res.send("Ajout de membre");
+});
+
+/**
+ * @swagger
+ * /groupe:
+ * delete:
+ * description: Utilisé pour supprimer un groupe
+ * responses:
+ * 
+ * 
+ */
+
+routerGroupe.delete("/", (req, res) => {
+  res.send("exclusion de membre");
+});
+
+/**
+ * @swagger
+ * /groupe:
+ * delete:
+ * description: Utilisé pour supprimer un groupe
+ * responses:
+ * 
+ * 
+ */
+
+routerGroupe.post("/", (req, res) => {
+  res.send("envoi de message");
+});
+
+/**
+ * @swagger
+ * /groupe:
+ * delete:
+ * description: Utilisé pour supprimer un groupe
+ * responses:
+ * 
+ * 
+ */
+
+routerGroupe.delete("/", (req, res) => {
+  res.send("suppression de message");
+});
+
+/**
+ * @swagger
+ * /groupe:
+ * delete:
+ * description: Utilisé pour supprimer un groupe
+ * responses:
+ * 
+ * 
+ */
+
+routerGroupe.get("/", (req, res) => {
+  res.send("recoit les messages");
+});
+
+/**
+ * @swagger
+ * /groupe:
+ * delete:
+ * description: Utilisé pour supprimer un groupe
+ * responses:
+ * 
+ * 
+ */
+
+routerGroupe.delete("/", (req, res) => {
+  res.send("quitter le groupe");
+});
+
 module.exports = routerGroupe;
