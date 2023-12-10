9e66d77533ae0856fdda403db8d7fa336460850a
NikoGandon <nikola.gandon@gmail.com>
2023-12-10 01:09:03 +0100

    WIP: création de routes pour le service Serveur

--- ServeurSide/app-express/src/Route/Serveur/serveur.js
+++ ServeurSide/app-express/src/Route/Serveur/serveur.js
@@ -7,13 +7,12 @@ const routerServeur = express.Router();
  * get:
  * description: Utilisé pour récupérer l'interface du serveur
  * responses:
- * 
+ *
  */
 
 routerServeur.get("/", (req, res) => {
   res.send("serveur page");
 });
-
 
 /**
  * @swagger
@@ -21,14 +20,13 @@ routerServeur.get("/", (req, res) => {
  * post:
  * description: Utilisé pour créer un serveur
  * responses:
- * 
- * 
+ *
+ *
  */
 
 routerServeur.post("/", (req, res) => {
   res.send("Création du serveur");
 });
-
 
 /**
  * @swagger
@@ -36,14 +34,13 @@ routerServeur.post("/", (req, res) => {
  * put:
  * description: Utilisé pour modifier un serveur
  * responses:
- * 
- * 
+ *
+ *
  */
 
 routerServeur.put("/", (req, res) => {
   res.send("Modification du serveur");
 });
-
 
 /**
  * @swagger
@@ -51,12 +48,138 @@ routerServeur.put("/", (req, res) => {
  * delete:
  * description: Utilisé pour supprimer un serveur
  * responses:
- * 
- * 
+ *
+ *
  */
 
 routerServeur.delete("/", (req, res) => {
   res.send("Suppression de serveur");
+});
+
+/**
+ * @swagger
+ * /serveur/ban:
+ * post:
+ * description: Utilisé pour bannir un utilisateur du serveur
+ * responses:
+ *
+ *
+ */
+
+routerServeur.post("/ban", (req, res) => {
+  res.send("Bannissement d'un utilisateur");
+});
+
+/**
+ * @swagger
+ * /serveur/ban:
+ * delete:
+ * description: Utilisé pour débannir un utilisateur du serveur
+ * responses:
+ *
+ *
+ */
+
+routerServeur.delete("/ban", (req, res) => {
+  res.send("Débannissement d'un utilisateur");
+});
+
+/**
+ * @swagger
+ * /serveur/salon:
+ * get:
+ * description: Utilisé pour récupérer l'interface d'un salon
+ * responses:
+ *
+ *
+ */
+
+routerServeur.get("/salon", (req, res) => {
+  res.send("Récupération de l'interface du salon");
+});
+
+/**
+ * @swagger
+ * /serveur/salon:
+ * post:
+ * description: Utilisé pour créer un salon
+ * responses:
+ *
+ *
+ */
+
+routerServeur.post("/salon", (req, res) => {
+  res.send("Création d'un salon");
+});
+
+/**
+ * @swagger
+ * /serveur/salon:
+ * put:
+ * description: Utilisé pour modifier un salon
+ * responses:
+ *
+ *
+ */
+
+routerServeur.put("/salon", (req, res) => {
+  res.send("Modification d'un salon");
+});
+
+/**
+ * @swagger
+ * /serveur/salon:
+ * delete:
+ * description: Utilisé pour supprimer un salon
+ * responses:
+ *
+ *
+ */
+
+routerServeur.delete("/salon", (req, res) => {
+  res.send("Suppression d'un salon");
+});
+
+/**
+ * @swagger
+ * /serveur/salon/invite:
+ * post:
+ * description: Utilisé pour créer un lien d'invitation
+ * responses:
+ *
+ *
+ */
+
+routerServeur.post("/salon/invite", (req, res) => {
+  res.send("Création d'un lien d'invitation");
+});
+
+/**
+ * @swagger
+ * /serveur/salon/invite:
+ * get:
+ * description: Utilisé pour récupérer tous les liens d'invitations
+ * responses:
+ *
+ *
+ */
+
+routerServeur.get("/salon/invite", (req, res) => {
+  res.send("Récupération de tous les liens d'invitations");
+});
+
+/**
+ * @swagger
+ * /serveur/salon/invite:
+ * delete:
+ * description: Utilisé pour supprimer un lien d'invitation
+ * responses:
+ *
+ *
+ */
+
+routerServeur.delete("/salon/invite", (req, res) => {
+  res.send("Suppression d'un lien d'invitation");
 });
 
 module.exports = routerServeur;
