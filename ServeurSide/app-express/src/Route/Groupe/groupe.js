70ee0b03a55d1b638cd8006ce486b9a66fa34f66
NikoGandon <nikola.gandon@gmail.com>
2023-12-10 01:36:37 +0100

    WIP: importation de modèle pour Groupe

--- ServeurSide/app-express/src/Route/Groupe/groupe.js
+++ ServeurSide/app-express/src/Route/Groupe/groupe.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const routerGroupe = express.Router();
 
+const groupeModel = require("../../Model/Groupe.model");
+const MembreGroupeModel = require("../../Model/Lien/MembreGroupe.model");
+
 /**
  * @swagger
  * /groupe:
