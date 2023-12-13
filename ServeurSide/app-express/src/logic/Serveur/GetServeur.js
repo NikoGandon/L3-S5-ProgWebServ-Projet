d73eb1ce04303080ca0a049caf25a3869150585b
Thomas <tailleurt@gmail.com>
2023-12-13 13:42:10 +0100

    WIP - GetServeur

--- ServeurSide/app-express/src/logic/Serveur/GetServeur.js
+++ ServeurSide/app-express/src/logic/Serveur/GetServeur.js
@@ -11,7 +11,13 @@ async function GetServeur(req, res) {
         return res.status(404).json({ error: "Serveur non trouvé." });
       }
   
-      return res.status(404).json({ error: "Serveur trouvé." });
+      return res.status(200).json({
+        id: serveur.id,
+        nom: serveur.nom,
+        description: serveur.description,
+        lienParametre: serveur.lienParametre,
+        lienLog: serveur.lienLog,
+      });
   
     } catch (error) {
       return res.status(500).json({ error: "Erreur lors de la récupération du serveur."  + error });
