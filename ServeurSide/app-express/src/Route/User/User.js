585e450b924fe702bf63a677f0578b6c4c1b6f43
NikoGandon <nikola.gandon@gmail.com>
2023-12-15 13:26:15 +0100

    WIP: bug

--- ServeurSide/app-express/src/Route/User/User.js
+++ ServeurSide/app-express/src/Route/User/User.js
@@ -17,7 +17,7 @@ const userLogic = require("../../logic/Utilisateur/user");
  *
  */
 routeurUser.get("/", (req, res) => {
-  userLogic.getInformation(req, res);
+  return userLogic.getInformation(req, res);
 });
 
 /**
@@ -30,7 +30,7 @@ routeurUser.get("/", (req, res) => {
  */
 
 routeurUser.put("/", (req, res) => {
-  userLogic.updateInformation(req, res);
+  return userLogic.updateInformation(req, res);
 });
 
 /**
