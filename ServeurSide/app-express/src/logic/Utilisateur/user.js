a5b4f073effd5bd10e9f6b38ad1beb8a685f25b8
NikoGandon <nikola.gandon@gmail.com>
2023-12-15 15:05:32 +0100

    feat: update bio de l'utilisateur connecté

--- ServeurSide/app-express/src/logic/Utilisateur/user.js
+++ ServeurSide/app-express/src/logic/Utilisateur/user.js
@@ -1,4 +1,4 @@
-const UserModel = require('../../Model/User.model');
+const UserModel = require("../../Model/User.model");
 
 const { infoToken } = require("../../Middleware/AuthToken");
 
@@ -26,39 +26,50 @@ async function getInformation(req, res) {
 }
 
 async function updateInformation(req, res) {
-    const { username, email, password } = req.body;
-    const token = infoToken(req);
-    const id = token.id;
-    const user = await UserModel.findOne({ id: id });
-    if (!user) {
-        return res.status(401).json({ message: "Utilisateur non trouvé." });
-    }
+  const { username, email, password, bio } = req.body;
+  const token = infoToken(req);
+  const id = token.id;
+  const user = await UserModel.findOne({ id: id });
+  if (!user) {
+    return res.status(401).json({ message: "Utilisateur non trouvé." });
+  }
+  try {
     if (username) {
-        user.username = username;
+      user.username = username;
     }
     if (email) {
-        user.email = email;
+      user.email = email;
     }
     if (password) {
-        user.password = password;
+      user.password = password;
+    }
+    if (bio){
+        user.bio = bio;
     }
     await user.save();
     return res.status(200).json({ message: "Utilisateur modifié." });
+  } catch (error) {
+    return res.status(500).json({ message: "Erreur interne." });
+  }
 }
 
 async function deleteUser(req, res) {
-    const token = infoToken(req);
-    const id = token.id;
-    const user = await UserModel.findOne({ id: id });
-    if (!user) {
-        return res.status(401).json({ message: "Utilisateur non trouvé." });
-    }
+  const token = infoToken(req);
+  const id = token.id;
+  const user = await UserModel.findOne({ id: id });
+  if (!user) {
+    return res.status(401).json({ message: "Utilisateur non trouvé." });
+  }
+  try {
     await user.delete();
-    return res.status(200).json({ message: "Utilisateur supprimé." });
+  } catch (error) {
+    return res.status(500).json({ message: "Erreur interne." });
+  }
+  return res.status(200).json({ message: "Utilisateur supprimé." });
 }
 
 module.exports = {
-    getInformation,
-    updateInformation,
-    deleteUser
-}
+  getInformation,
+  updateInformation,
+  deleteUser,
+};
