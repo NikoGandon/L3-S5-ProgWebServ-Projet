86469dc2f9962b54b1c80b2844405ace1be8e94e
NikoGandon <nikola.gandon@gmail.com>
2023-12-25 02:04:46 +0100

    WIP: OAuth2

--- ClientSide/app-react/src/components/authentification/OAuth2.jsx
+++ ClientSide/app-react/src/components/authentification/OAuth2.jsx
@@ -1,9 +1,27 @@
-import { React } from "react";
+import React, { } from "react";
+import { GoogleLogin } from "@react-oauth/google";
 
 /**
  * @desc Bouton OAuth2 de google
  */
 
 export default function OAuth2() {
-  return (<div></div>);
+
+  const onSuccess = (response) => {
+    console.log(response);
+  };
+
+  const onFailure = (response) => {
+    console.log(response);
+  };
+
+  return (
+    <GoogleLogin>
+      clientId="683358252215-to6amlqd2qgr0rvq1629fn2g447luq2q.apps.googleusercontent.com"
+      buttonText="Login"
+      onSuccess={onSuccess}
+      onFailure={onFailure}
+      redirectUri="http://localhost:3000/auth"
+    </GoogleLogin>
+  );
 }
--- ClientSide/app-react/src/components/authentification/authentication.jsx
+++ ClientSide/app-react/src/components/authentification/authentication.jsx
@@ -1,8 +1,10 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 
 import Login from "./login";
 import Regiter from "./regiter";
 import OAuth2 from "./OAuth2";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 /**
  * @desc Formulaire de connexion ou d'inscription avec un bouton pour switcher entre les deux et un bouton pour se connecter avec Google
@@ -15,10 +17,6 @@ export default function Authentication() {
     setIsLogin(!isLogin);
   };
 
-  const handleGoogle = () => {
-    // TODO
-  };
-
   return (
     <div>
       {isLogin ? (
@@ -26,7 +24,9 @@ export default function Authentication() {
       ) : (
         <Regiter onSwitch={handleSwitch} />
       )}
+      <GoogleOAuthProvider clientId="683358252215-to6amlqd2qgr0rvq1629fn2g447luq2q.apps.googleusercontent.com">
+        <OAuth2 />
+      </GoogleOAuthProvider>
     </div>
   );
-
 }
