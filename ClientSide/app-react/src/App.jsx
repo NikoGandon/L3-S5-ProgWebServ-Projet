140de00c96f289bc5492a45191dbd64a82676ab2
NikoGandon <nikola.gandon@gmail.com>
2023-12-25 01:22:13 +0100

    WIP: gestion de token

--- ClientSide/app-react/src/App.jsx
+++ ClientSide/app-react/src/App.jsx
@@ -6,11 +6,14 @@ import "./App.css";
 import HomeUnconnected from "./components/home/homeUnconnected";
 import Home from "./components/Home/home";
 import Authentication from "./components/authentification/authentication";
+import Login from "./components/authentification/login";
+import Register from "./components/authentification/regiter";
 
 const elem = <h1>Bienvenue sur le site XXXX</h1>;
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem("token"));
+  console.log("token stocké  : " + token);
   const estConnecte = token !== null;
   if (token) {
     const { exp } = jwtDecode(token);
