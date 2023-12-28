85f2b64a0589a608f6c35a88787a70062c79e9b4
NikoGandon <nikola.gandon@gmail.com>
2023-12-28 14:26:37 +0100

    feat: envoie de token dans un cookie

--- ServeurSide/app-express/src/Route/Auth/login.js
+++ ServeurSide/app-express/src/Route/Auth/login.js
@@ -1,10 +1,9 @@
-const express = require("express");
-const routerLogin = express.Router();
+const routerLogin = require("express").Router();
 
 const passportLogin = require("../../Middleware/Passport/PassportLogin");
 const { createToken } = require("../../Middleware/AuthToken");
 
-routerLogin.post("/", (req, res, next) => {
+routerLogin.post("/", async (req, res, next) => {
   if (!req.body.username || !req.body.password) {
     res.status(400).json({
       message: "Veuillez remplir tous les champs",
@@ -17,30 +16,49 @@ routerLogin.post("/", (req, res, next) => {
         username: req.body.username,
         password: req.body.password,
       },
-      (err, user, info) => {
-        if (err) {
-          return res.status(400).json({
-            error: err,
-          });
+      async (err, user, info) => {
+        try {
+          if (err) {
+            console.log("err : " + err);
+            return res.status(400).json({
+              authStatus: false,
+              error: err,
+            });
+          }
+
+          if (!user) {
+            console.log("mess : " + info.message);
+            return res.status(400).json({
+              authStatus: false,
+              message: info.message,
+            });
+          }
+
+          const token = createToken(user);
+
+          let dateExpiration = new Date();
+          dateExpiration = dateExpiration.setDate(
+            dateExpiration.getMonth() + 6
+          );
+
+          console.log("token : " + token);
+
+          return res
+            .cookie("authToken", token, {
+              domain: "localhost",
+              path: "/",
+              httpOnly: true,
+              secure: true,
+              sameSite: "None",
+              maxAge: dateExpiration,
+            })
+            .json({
+              authStatus: true,
+              message: "Authentification réussi",
+            });
+        } catch (error) {
+          console.log(error);
         }
-
-        if (!user) {
-          return res.status(400).json({
-            message: info.message,
-          });
-        }
-
-        const token = createToken(user);
-
-        let dateExpiration = new Date();
-        dateExpiration = dateExpiration.setDate(expiresIn.getMonth() + 6);
-
-        return res.status(200).cookie("authToken", token, {
-          httpOnly: true,
-          secure: true,
-          sameSite: "none",
-          maxAge: new Date(Date.now().getMonth() + 6),
-        });
       }
     )(req, res, next);
   } catch (error) {
