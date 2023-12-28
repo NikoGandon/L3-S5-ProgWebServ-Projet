8fbe784d9690a1cdf1b8a4ab320ba316b42c728e
MOttet18 <113054042+MOttet18@users.noreply.github.com>
2023-12-28 19:05:31 +0100

    WIP: gestion de groupe
    récupération de groupe du user

--- ClientSide/app-react/src/components/groupe/groupe.jsx
+++ ClientSide/app-react/src/components/groupe/groupe.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./groupe.css";
+
+import axios from "../../utils/axiosConf";
 
 const Groupe = () => {
+
+  const [groupe, setGroupe] = useState();
+
+  useEffect(() => {
+    axios.get("https://localhost:3000/user/get-groupes").then((res) => {
+      console.log(res);
+      setGroupe(res.data.groupes);
+    });
+  }, []);
+
   return (
     <>
-      <h1>Groupe</h1>
+      <div className="container_grp">
+        <div className="LeftSide">
+        <button className="btn_base"><img className="icon_base" src="../../../images/porsche.jpg" /></button>
+          <div className="barre"></div>
+          <button className="btn_base"><img className="icon_base" src="../../../images/mp.png" /></button>
+          <div className="barre"></div>
+          <div className="box_serveur">
+          </div>
+        </div>
+        <div className="mp-groupe">
+          <input type="search" id="mp_search" placeholder="Recherche message ..."/>
+          <button className="btn_groupe" type="button">Amis</button>
+          <div className="barre2"></div>
+          <button className="btn_groupe" type="button">Tout</button>
+          <button className="btn_groupe" type="button">Privé</button>
+          <button className="btn_groupe" type="button">Groupe</button>
+          <div className="barre2"></div>
+          <div className="box_mp">
+            <button className="btn_find_groupe">
+            <div className="div_find_groupe">
+              <img className="icon_mp" src="../../../images/singe.jpg" />
+              <div className="name_mp_box">
+                <p className="name_mp">nom groupe</p>
+                <p className="name_mp">5 membres</p>
+              </div>
+            </div>
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
