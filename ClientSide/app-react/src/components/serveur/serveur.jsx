3cab9ac0d2e7807dc57017a4d65994662a0f1acf
NikoGandon <nikola.gandon@gmail.com>
2023-12-28 23:10:09 +0100

    WIP: récupère info serveur

--- ClientSide/app-react/src/components/serveur/serveur.jsx
+++ ClientSide/app-react/src/components/serveur/serveur.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+/**
+ * @desc Affiche l'interface d'un serveur (liste des salons, liste des membres, liste des messages)
+ */
 const Serveur = () => {
+  const [idServeur, setIdServeur] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("https://localhost:3000/serveur/", {
+        params: {
+          id: idServeur,
+        },
+      })
+      .then((res) => {});
+  }, []);
+
   return (
     <>
       <h1>Serveur</h1>
