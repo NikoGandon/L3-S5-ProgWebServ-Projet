b00d27e2195cfb35e6db8d431e4b2afd3cf7eb2d
NikoGandon <nikola.gandon@gmail.com>
2023-12-28 23:10:21 +0100

    refactor: ajout div pour structure

--- ClientSide/app-react/src/components/home/home.jsx
+++ ClientSide/app-react/src/components/home/home.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import  Content  from "./sectionHome/content";
-import  ServeurBar  from "./sectionHome/serveurBar";
-import  BarreLat  from "./sectionHome/barreLat";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import Content from "./sectionHome/content";
+import ServeurBar from "./sectionHome/serveurBar";
+import BarreLat from "./sectionHome/barreLat";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
       <h1>Bienvenue à toi !</h1>
-      <ServeurBar />
-      <BarreLat />
-      <Content />
+      <div id="barreServeur">
+        <ServeurBar />
+      </div>
+      <div id="barreLaterale">
+        <BarreLat />
+      </div>
+      <div id="content">
+        <Content />
+      </div>
     </>
   );
 };
