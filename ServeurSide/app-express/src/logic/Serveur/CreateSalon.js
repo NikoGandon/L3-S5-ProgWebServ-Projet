d9515d781843f4c7f2fe0a0952087270911345fd
NikoGandon <nikola.gandon@gmail.com>
2023-12-28 22:43:23 +0100

    feat: envoie de message de l'API

--- ServeurSide/app-express/src/logic/Serveur/CreateSalon.js
+++ ServeurSide/app-express/src/logic/Serveur/CreateSalon.js
@@ -9,21 +9,19 @@ const SalonModel = require("../../Model/Salon.model");
 
 async function CreateSalon(req, res) {
   try {
-    const id = req.body.id;
     const nom = req.body.nom;
     const description = req.body.description;
     const idServeur = req.body.idServeur;
 
     const NewSalon = await SalonModel.create({
-      id: id,
       nom: nom,
       description: description,
       idServeur: idServeur,
     });
 
-    return res.status(201).json(NewSalon);
+    return res.status(201).json({ message: "Salon créé." });
   } catch (error) {
-    return res.status(500).json({ error: "Ca marche pas." + error });
+    return res.status(500).json({ error: "Erreur lors de la création du salon."});
   }
 }
 
