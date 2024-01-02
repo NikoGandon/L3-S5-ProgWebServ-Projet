29d23e1499dd3c29de61a62a8608b558d3f256c3
NikoGandon <nikola.gandon@gmail.com>
2024-01-02 05:10:54 +0100

    WIP: récupérer et afficher DM

--- ClientSide/app-react/src/components/MP/mp.jsx
+++ ClientSide/app-react/src/components/MP/mp.jsx
@@ -1,13 +1,38 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 
 import axios from "../../utils/axiosConf";
 import { UserContext } from "../../contexts/user.context";
+
+const OwnMessage = ({ message, username, lienPP }) => {
+  return (
+    <div className="own_message">
+      <p className="own_message_text">{message}</p>
+      <div className="userA">
+        <p className="username">{username}</p>
+        <img className="iconMessage" src={lienPP} />
+      </div>
+    </div>
+  );
+};
+
+const OtherMessage = ({ message, username, lienPP }) => {
+  return (
+    <div className="other_message">
+      <p className="other_message_text">{message}</p>
+      <div className="userB">
+        <img className="iconMessage" src={lienPP} />
+        <p className="username">{username}</p>
+      </div>
+    </div>
+  );
+};
 
 /**
  * @desc Affiche l'interface d'un message privé (liste des messages ainsi que le nom de l'interlocuteur)
  */
 const MessagePrv = () => {
   const { contexteID } = useContext(UserContext);
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     axios
@@ -16,12 +41,38 @@ const MessagePrv = () => {
           id: contexteID,
         },
       })
-      .then((res) => {});
+      .then((res) => {
+        setMessages(res.data.messages);
+      });
   }, []);
 
   return (
     <>
-      <h1>MessagePrv</h1>
+      <div className="message_prv">
+        <div className="messages">
+          {messages.map((message) => {
+            if (message.isOwnMessage) {
+              return (
+                <OwnMessage
+                  key={message.idMessagePrv}
+                  message={message.message.contenu}
+                  username={message.message.Auteur.nom}
+                  lienPP={message.message.Auteur.lienPP}
+                />
+              );
+            } else {
+              return (
+                <OtherMessage
+                key={message.idMessagePrv}
+                message={message.message.contenu}
+                username={message.message.Receveur.nom}
+                lienPP={message.message.Receveur.lienPP}
+                />
+              );
+            }
+          })}
+        </div>
+      </div>
     </>
   );
 };
