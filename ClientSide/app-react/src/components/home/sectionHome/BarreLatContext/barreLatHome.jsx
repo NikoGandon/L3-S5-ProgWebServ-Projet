fe082e2f5855ab63450f3696cf8dc491a54967e8
NikoGandon <nikola.gandon@gmail.com>
2024-01-02 02:39:19 +0100

    WIP: affichage des derniers Messages/Groupes de discussion

--- ClientSide/app-react/src/components/home/sectionHome/BarreLatContext/barreLatHome.jsx
+++ ClientSide/app-react/src/components/home/sectionHome/BarreLatContext/barreLatHome.jsx
@@ -26,40 +26,98 @@ const GroupeBox = ({ nomGroupe, nbMembres, handleClick }) => {
 };
 
 /**
+ * @desc Section d'un message privé
+ * @returns
+ */
+const MessagePriveBox = ({ nomUtilisateur, handleClick }) => {
+  return (
+    <button
+      className="btn_find_groupe"
+      onClick={() => {
+        handleClick("mp", id);
+      }}
+    >
+      <div className="div_find_groupe">
+        <img className="icon_mp" src="../../../images/singe.jpg" />
+        <div className="name_mp_box">
+          <p className="name_mp">{nomUtilisateur}</p>
+        </div>
+      </div>
+    </button>
+  );
+};
+
+/**
  * @desc Affiche tous les Groupes et MP dont l'utilisateur est membre
  *       trié par ordre chronologique
  */
 const BarreLatHome = ({ handleClick }) => {
-  // TODO : Récupérer les MP de l'utilisateur
-  // TODO : Trier par ordre chronologique
   const [groupesMembre, setGroupesMembre] = useState([]);
+  const [messagesPrives, setMessagesPrives] = useState([]);
+  const [mergedList, setMergedList] = useState([]);
 
   useEffect(() => {
     axios
-      .get("https://localhost:3000/user/get-groupes")
+      .get("https://localhost:3000/user/get-DM")
       .then((res) => {
         console.log(res.data);
-        setGroupesMembre(res.data.Groupes);
+
+        const groupesTriés = res.data.groupes.sort((a, b) => {
+          const dateA = new Date(a.messages[0]?.message.createdAt || 0);
+          const dateB = new Date(b.messages[0]?.message.createdAt || 0);
+          return dateB - dateA;
+        });
+
+        const messagesTriés = res.data.messagesPrives.sort((a, b) => {
+          const dateA = new Date(a.message.createdAt);
+          const dateB = new Date(b.message.createdAt);
+          return dateB - dateA;
+        });
+
+        setGroupesMembre(groupesTriés);
+        setMessagesPrives(messagesTriés);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
+  useEffect(() => {
+    const mergedList = [...groupesMembre, ...messagesPrives];
+
+    mergedList.sort((a, b) => {
+      const dateA = new Date(a.message.createdAt || 0);
+      const dateB = new Date(b.message.createdAt || 0);
+      return dateB - dateA;
+    });
+
+    setMergedList(mergedList);
+  }, [groupesMembre, messagesPrives]);
+
   return (
     <>
       <div className="barreLatHome">
         <div className="box_mp">
-          {groupesMembre.length > 0
-            ? groupesMembre.map(({ nom, nbMembres, id }) => (
+          {mergedList.length > 0 ? (
+            mergedList.map((item) =>
+              item.nom ? (
                 <GroupeBox
-                  key={id}
-                  nomGroupe={nom}
-                  nbMembres={nbMembres}
+                  key={item.id}
+                  nomGroupe={item.nom}
+                  nbMembres={item.nbMembres}
                   handleClick={handleClick}
                 />
-              ))
-            : null}
+              ) : (
+                <MessagePriveBox
+                  key={item.id}
+                  nomUtilisateur={item.nomUtilisateur}
+                  handleClick={handleClick}
+                />
+              )
+            )
+          ) : (
+            <p>Commencez à discuter !</p>
+          )}
         </div>
       </div>
     </>
