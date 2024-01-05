a9ed9e3ce8ee0371c906a0e8f0f8582925ce7d17
NikoGandon <nikola.gandon@gmail.com>
2024-01-05 02:16:47 +0100

    WIP: page d'un serveur

--- ClientSide/app-react/src/components/serveur/serveur.jsx
+++ ClientSide/app-react/src/components/serveur/serveur.jsx
@@ -3,12 +3,29 @@ import React, { useState, useEffect, useContext } from "react";
 import axios from "../../utils/axiosConf";
 import { UserContext } from "../../contexts/user.context";
 
+import ConversationChat from "../chat/ConversationChat";
+import BarreChat from "../chat/BarreChat";
+
+const MemberList = ({ membres }) => {
+  return (
+    <>
+      {membres.map((membre) => {
+        return (
+          <div key={membre.id}>
+            <p>{membre.pseudo}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
 /**
  * @desc Affiche l'interface d'un serveur (liste des salons, liste des membres, liste des messages)
  */
 const Serveur = () => {
   const { contexteID, contexteSalon } = useContext(UserContext);
+  const [membres, setMembres] = useState([]);
 
   useEffect(() => {
     axios
@@ -18,11 +35,24 @@ const Serveur = () => {
           idSalon: contexteSalon,
         },
       })
+      .then((res) => {
+        setMembres(res.data.membres);
+      });
   }, [contexteID, contexteSalon]);
 
   return (
     <>
-      <h1>Serveur</h1>
+      <h1 className="titleNameServeur"></h1>
+      <div className="memberList">
+        <MemberList membres={membres} />
+      </div>
+
+      <div className="messageList">
+        <ConversationChat />
+      </div>
+      <div className="barreChat">
+        <BarreChat />
+      </div>
     </>
   );
 };
