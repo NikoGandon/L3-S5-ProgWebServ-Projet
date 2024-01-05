b3f43d8fc0010870261d98c394146ee78612a10d
NikoGandon <nikola.gandon@gmail.com>
2024-01-05 02:16:16 +0100

    WIP: ajout du contexteSalon

--- ClientSide/app-react/src/components/chat/BarreChat.jsx
+++ ClientSide/app-react/src/components/chat/BarreChat.jsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect, useContext } from "react";
-import axios from "axios";
 import { UserContext } from "../../contexts/user.context";
 import { io } from "socket.io-client";
 
-const FormChat = () => {
-  const { contexteUser, contexteID } = useContext(UserContext);
+/**
+ * @desc Logique pour le formulaire de chat
+ * @param {*} sendMessage
+ * @returns
+ */
+const FormChat = ({ sendMessage }) => {
   const [message, setMessage] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    sendMessage(message);
     setMessage("");
   };
 
@@ -28,9 +32,12 @@ const FormChat = () => {
   );
 };
 
+/**
+ * @desc Affiche une barre de chat, utilise le contexte pour savoir si on est dans un groupe ou un serveur et pour rejoindre au bon socket
+ * @returns
+ */
 const Chat = () => {
-  const { contexteUser, contexteID } = useContext(UserContext);
-  const [message, setMessage] = useState("");
+  const { contexteUser, contexteID, contexteSalon } = useContext(UserContext);
 
   useEffect(() => {
     const socket = io("https://localhost:3000");
@@ -48,7 +55,7 @@ const Chat = () => {
 
   return (
     <>
-      <FormChat />
+      <FormChat sendMessage={sendMessage} />
     </>
   );
 };
