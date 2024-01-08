cdc7ffb6708c22164a9599e0a71054841ebfe01d
MOttet18 <113054042+MOttet18@users.noreply.github.com>
2024-01-09 00:35:47 +0100

    WIP: parm user

--- ClientSide/app-react/src/components/user/param.jsx
+++ ClientSide/app-react/src/components/user/param.jsx
@@ -2,9 +2,10 @@ import React ,{useState} from "react";
 import axios from "../../utils/axiosConf";
 
 const Param = () => {
-  const [identifier, setidentifier] = useState("");
+  const [identifier, setidentifier] = useState(null);
   const [email, setemail] = useState(null);
   const [bio, setbio] = useState(null);
+  const [code, setCode] = useState(null);
 
   const handleidentifierChange = (event) => {
     setidentifier(event.target.value);
@@ -15,6 +16,9 @@ const Param = () => {
   const handlebioChange = (event) => {
     setbio(event.target.value);
   };
+  const handleCodeChange = (event) => {
+    setCode(event.target.value);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -22,7 +26,8 @@ const Param = () => {
       .put("https://localhost:3000/User", {
         username: identifier,
         email: email,
-        bio: bio
+        bio: bio,
+        password: code
       })
       .then((response) => {
         window.location.reload(true);
@@ -40,13 +45,34 @@ const Param = () => {
     <>
       <h1>Param-User</h1>
       <form className="formsParam" onSubmit={handleSubmit}>
-      <label htmlFor="identifier">new identifiant</label>
+      <label htmlFor="identifier">Nouveau Pseudo: </label>
         <input
           type="identifier"
           id="identifier"
           value={identifier}
           onChange={handleidentifierChange}
-        />
+        /><br></br>
+      <label htmlFor="email">Nouveau Email: </label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleemailChange}
+        /><br></br>
+      <label htmlFor="bio">Nouvelle bio: </label>
+        <input
+          type="bio"
+          id="bio"
+          value={bio}
+          onChange={handlebioChange}
+        /><br></br>
+      <label htmlFor="password">Nouveau Code: </label>
+        <input
+          type="password"
+          id="password"
+          value={code}
+          onChange={handleCodeChange}
+        /><br></br>
         <button type="submit">Valider</button>
       </form>
     </>
