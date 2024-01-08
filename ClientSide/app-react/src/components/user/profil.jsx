632b552d7ee0721072334be8056f742cc5caa7df
NikoGandon <nikola.gandon@gmail.com>
2024-01-08 01:47:56 +0100

    style

--- ClientSide/app-react/src/components/user/profil.jsx
+++ ClientSide/app-react/src/components/user/profil.jsx
@@ -1,24 +1,39 @@
-import React from "react";
-import PopUp from "../pop-up/pop-up.model"
-import axios from "../../utils/axiosConf"
+import React, { useEffect, useState, useContext } from "react";
+import PopUp from "../pop-up/pop-up.model";
+import axios from "../../utils/axiosConf";
 
 const Profil = () => {
   const [users, setUser] = useState();
   const [profilSelectionne, setProfilSelectionne] = useState(null);
-  const [estAmi, setEstAmi,groupeExiste, setGroupeExiste] = useState(false);
-  useEffect(() => {
-    getFriend(userId)
-      .then((estAmi) => setEstAmi(estAmi))
-      .catch((error) => console.error('Erreur lors de la vérification de l\'ami : ', error)); [userId, getFriend];
+  const [estAmi, setEstAmi] = useState(false);
 
-      const groupesFiltres = chatGroups.filter(groupeId => groupeId.MembreGroupe.length === 2);
-      setGroupesDeuxUtilisateurs(groupesFiltres); [chatGroups];
-    
-    
-    axios.get('https://localhost:3000/serveur/')
+  useEffect(() => {
+    axios
+      .get("https://localhost/????", {})
+      .then((estAmi) => setEstAmi(estAmi))
+      .catch((error) =>
+        console.error("Erreur lors de la vérification de l'ami : ", error)
+      );
+    [userId, getFriend];
+
+    const groupesFiltres = chatGroups.filter(
+      (groupeId) => groupeId.MembreGroupe.length === 2
+    );
+
+    setGroupesDeuxUtilisateurs(groupesFiltres);
+    [chatGroups];
+
+    axios
+      .get("https://localhost:3000/serveur/")
       .then((response) => setUser(response.data))
-      .catch((error) => console.error('Erreur lors de la récupération des utilisateurs : ', error));
+      .catch((error) =>
+        console.error(
+          "Erreur lors de la récupération des utilisateurs : ",
+          error
+        )
+      );
   }, []);
+
   /*const handleCloseProfil = () => {
     setProfilSelec(null);
   };*/
@@ -26,26 +41,36 @@ const Profil = () => {
     if (!estAmi) {
       addFriend(userId)
         .then(() => setEstAmi(true))
-        .catch((error) => console.error('Erreur lors de l\'ajout d\'ami : ', error));
+        .catch((error) =>
+          console.error("Erreur lors de l'ajout d'ami : ", error)
+        );
     }
   };
+
   const TestEnvoyerMessage = (groupeId) => {
-    // Redirection vers la page de discussion du groupe avec l'ID groupeId
+    // A changer avec les contextes
+
     window.location.href = `/discussion/${groupeId}`;
   };
-    return (
+
+  return (
     <PopUp>
       <h1>Profil : {User.username}</h1>
       <div className="userinfo">
-      {//ProfilSelec && (<Popup utilisateur={ProfilSelec} onClose={handleCloseProfil} />)}}
-}
+        {
+          //ProfilSelec && (<Popup utilisateur={ProfilSelec} onClose={handleCloseProfil} />)}}
+        }
         <p> {User.lienPP}</p>
         <p>Date d'inscription : {User.createdAt}</p>
         <p>Bio : {User.Bio}</p>
         {estAmi ? (
-         <button disabled>Déjà Ami</button>) :(<button onClick={testAjouterAmi}>Ajouter en ami</button>)}
-         <button onClick={() => TestEnvoyerMessage(groupeId)}>Envoyer un message</button>
-        
+          <button disabled>Déjà Ami</button>
+        ) : (
+          <button onClick={testAjouterAmi}>Ajouter en ami</button>
+        )}
+        <button onClick={() => TestEnvoyerMessage(groupeId)}>
+          Envoyer un message
+        </button>
       </div>
     </PopUp>
   );
