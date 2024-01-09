b7d15b5816ce31413b3d74f804de5ef38bb11a2b
Nikola GANDON <nikola.gandon@etu.univ-tours.fr>
2024-01-09 00:38:59 +0000

    Revert "Merge branch 'dev' into 'main'"
    This reverts merge request !15

--- ServeurSide/app-express/src/Route/Auth/Auth.js
+++ ServeurSide/app-express/src/Route/Auth/Auth.js
@@ -17,7 +17,7 @@ routerAuth.get("/", (req, res) => res.status(200).json({ message: "Route non ter
 routerAuth.use("/login", login);
 routerAuth.use("/register", register);
 routerAuth.use("/logout", logout);
-routerAuth.use("/OAuth", OAuth);
+//routerAuth.use("/OAuth", OAuth);
 routerAuth.use("/OAuth", (req, res) => res.status(200).json({ message: "Route non terminée." }));
 
 module.exports = routerAuth;
