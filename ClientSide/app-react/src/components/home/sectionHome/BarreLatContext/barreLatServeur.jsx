2c322eba01b8cffbad828767512e2ed6730b0c50
Thomas <tailleurt@gmail.com>
2024-01-10 14:15:35 +0100

    wip: salon

--- ClientSide/app-react/src/components/home/sectionHome/BarreLatContext/barreLatServeur.jsx
+++ ClientSide/app-react/src/components/home/sectionHome/BarreLatContext/barreLatServeur.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import axios from "../../../../utils/axiosConf";
 import { UserContext } from "../../../../contexts/user.context";
-import Serveur from "../../../serveur/serveur";
-
-import AddMembre from "../../../serveur/addMembreServ";
+import CreateSalon from "../../../salon/createSalon";
 
 /**
  * @desc Récupère les salons d'un serveur
@@ -15,7 +13,7 @@ const barreLatServeur = ({ handleClick }) => {
   const { contexteUser, contexteID, contexteSalon, handleServeurSelect } =
     useContext(UserContext);
   const [salons, setSalons] = useState([]);
-  const idServ = contexteID;
+
   useEffect(() => {
     axios
       .get("https://localhost:3000/serveur/get-salons", {
@@ -41,19 +39,21 @@ const barreLatServeur = ({ handleClick }) => {
     handleServeurSelect(contexteID, idSalon);
   };
 
-  console.log("----- :: ", contexteSalon, " :: -----", "----- :: ", contexteID, " :: -----", "----- :: ", contexteUser, " :: -----");
-
   return (
     <>
     <div id="nomDuServeurDiv">
       <h3 id="nomServeurTitle">Serveur</h3>
       </div>
-      <div><AddMembre idServ={idServ}/></div>
+      <button id="bouttonMembre" >
+          <img src="../../public/image/ajouterMembre.png" width="35px" height="35px"></img>
+          <p id="textMembre">Ajouter</p>
+         </button>
          <button id="bouttonParametres">
           <img src="../../public/image/Paramètres.png" width="35px" height="35px"></img>
           <p id="textParametres">Paramètres</p>
          </button>
          <p id="conversation">Salons</p>
+         <CreateSalon/>
 
       {salons.length > 0
         ? salons.map((salon) => {
@@ -71,7 +71,7 @@ const barreLatServeur = ({ handleClick }) => {
               </div>
             );
           })
-        : "Aucun salon"}
+        : <p>Aucun salon ✘</p>}
     </>
   );
 };
